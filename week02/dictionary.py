837d9d8c14e109f07edc18d275efbce2890e2cc6
Dennis Gann <dg@dcg.nz>
2019-02-24 20:58:17 +1100

    week 2 starter python

---
+++ week02/dictionary.py
@@ -0,0 +1,23 @@
+'''
+Another useful data type built into Python is the dictionary. Unlike a list, string or tuple whose elements can be indexed by a range of numbers, dictionaries are indexed by keys, which can be any immutable type; strings and numbers can always be keys.
+
+phone = {
+        ‘sam’: 97896000,
+        ‘john’:9584 2384,
+        ‘tom’:97268907
+        }
+
+Common list methods:
+- list(d.keys()) on a dictionary returns all the keys in the dictionary.  
+use the 'in' keyword to check if a particular key exists
+- dict() can be used to build a dictionary from a sequence of key-value pairs
+- use items() to loop through the dictionary
+
+You are given a dictionary of fruits names and their prices ($). Add a new entry: ‘golden apple’, with a price of $42 per unit. Implement a function that does the following:
+
+1. For each fruit in store, ask the user how many units of the fruit the user would like to purchase (in any order)
+2. After the user has entered the units for every fruit, calculates the total price the user has to pay and prints this value out
+'''
+
+fruit_prices = {'bananas': 10, 'rock melons': 4, 'blueberries': 23}
+fruit_prices['golden apple'] = 42
---
+++ week02/lists.py
@@ -0,0 +1,12 @@
+'''
+The most versatile data structure, written as a list of comma separated values within square brackets. Lists might contain items of the same or different data types, e.g.,
+
+ - primes = [2,3,5,7,11,13]
+ - mixed_basket = [1,[apples,3,3.45],2,[oranges,4.05]]
+
+Given a list of numbers find the median of the numbers. You may assume that the input list contains at least one number.
+
+Think about why you many want to use sorted(numbers) instead of numbers.sort()
+'''
+numbers = [2, 4, -23, 2, 95, 21]
+
