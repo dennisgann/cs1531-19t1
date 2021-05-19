b3a818fcefa355913bbd322530bb83d3069c1729
Dennis Gann <dg@dcg.nz>
2021-05-19 15:13:23 +1000

    week 8

---
+++ week08/part01/example01/routes.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect, render_template, request, url_for
+from server import app
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        name = request.form["name"]
+        zID = int(request.form["zID"])
+        desc = request.form["desc"]
+        return redirect(url_for("hello", name=name, id=zID, desc=desc))
+    return render_template("index.html")
+
+
+@app.route("/Hello/<name>/<id>/<desc>")
+def hello(name, id, desc):
+    return render_template("hello.html", name=name, id=id, desc=desc)
